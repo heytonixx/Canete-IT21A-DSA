a68ba4dea8ed369aac3d277d10512e605694784d
heytonixx <dongkiecanete0@gmail.com>
2024-10-11 10:08:29 +0800

    added Abstraction.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ class Student extends Person {
     }
 }
 
-const student = new Student('Antony', 19, 'Student', 'Object Oriented  Programming');
+const student = new Student('March', 24, 'Instructor', 'Object Oriented  Programming');
 
 console.log("Student Name: " + student.getName());
 console.log("Age: " + student.getAge());
